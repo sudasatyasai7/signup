535496bc6824b1acd8759889c0679d3b0cf1a110
sudasatyasai7 <satyasai9883@gmail.com>
2024-07-14 11:37:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import LoginSignup from './Assets/loginsignup'
+const App = () => {
+  return (
+    <div id='main'>
+      <LoginSignup/>
+    </div>
+  );
+}
+
+export default App
---
+++ src/Assets/loginsignup.jsx
@@ -0,0 +1,34 @@
+import React,{useState} from 'react'
+const LoginSignup = () => {
+    const[data,setData]=useState("")
+    if(data==='login'){
+        return(
+        <div id='bod'>
+            <center id='aj'>
+            <form>
+             <input type='email' placeholder='Email'/><br/> <br/>
+             <input type='password' placeholder='Password'/><br/> <br/>
+             <button type='submit' onClick={()=>{setData('signup')}} id='sign'>Signup</button> &nbsp;<button type='submit' onClick={()=>{setData('login')}}>Login</button>
+          
+            </form>
+            </center>
+        </div>
+        );
+    }
+  return (
+    <div>
+      <center>
+        <h1>Sign Up</h1>
+        <form>
+          <input type='text' placeholder='Name'/> <br/> <br/>
+          <input type='email' placeholder='Email'/><br/> <br/>
+          <input type='password' placeholder='Password'/><br/> <br/>
+          <button type='submit' onClick={()=>{setData('signup')}} >Signup</button> <button type='submit' onClick={()=>{setData('login')}} id='log'>Login</button>
+          
+        </form>
+      </center>
+    </div>
+  );
+}
+
+export default LoginSignup
